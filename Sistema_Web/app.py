014460bf498cd3bd01d4689f4cfd1b970c801be0
HEllanlins <hellan.lins@gmail.com>
2024-10-17 09:38:00 -0300

    port 8000

--- Sistema_Web/app.py
+++ Sistema_Web/app.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello(): 
-    return 'hello world'
+    return 'Página Inicial '
+
+@app.route('/sobre')
+def sobre():
+    return 'Página sobre'
 
 if __name__ == '__main__':
+    app.run(port=8000)
     app.run(debug = True)
     
