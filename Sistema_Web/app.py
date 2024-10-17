1de0ce1096246044575a08becc5974cff8c4aad6
HEllanlins <hellan.lins@gmail.com>
2024-10-17 10:37:46 -0300

    chamar nome python

--- Sistema_Web/app.py
+++ Sistema_Web/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello(): 
-    return render_template('Pagina.html')
+    user = 'Hellan Lins'
+    return render_template('Pagina.html', user = user)
 
 @app.route('/sobre')
 def sobre():
