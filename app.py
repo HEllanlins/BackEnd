d3a1fe37198719dd1468c357617ebbf4071fb902
HEllanlins <hellan.lins@gmail.com>
2024-10-31 10:43:20 -0300

    alterações

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
@@ -17,12 +17,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'in
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 class Usuario(db.Model, UserMixin):
-    id = db.column(db.integer, primary_key=true)
-    nome = db.Column(db.sftring(100), nullable=false)
-    email = db.Column(db.sftring(100), nullable=false)
-    senha = db.Column(db.sftring(50), nullable=false)
+    id = db.column(db.Integer, primary_key=True) 
+    nome = db.Column(db.String(100), nullable=False) 
+    email = db.Column(db.Sftring(100), nullable=False) 
+    senha = db.Column(db.Sftring(50), nullable=False) 
+
+class Evento(db.Model):
+    id = db.column(db.Integer, primary_key=True) 
+    titulo = db.column(db.String(100), nullabel=False) 
+    descricao = db.column(db.Text, nullabel=False)
+    data_evento = db.column(db.Date, nullabel=False) 
+    usuario_id = db.column(db.Integer, db.ForeignKey('usuario.id'), nullabel=False)
     
 
 @app.route('/')
